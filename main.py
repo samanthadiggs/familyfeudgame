b39a65600f4e1913efbce72248759dc90ae9f1e8
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2024-01-19 08:12:54 -0500

    changed one line

--- main.py
+++ main.py
@@ -30,8 +30,9 @@ while gameOn == True:
     print("Welcome to Family Feud!  Each game has three rounds plus a fast money round!\n")
     game_selection = input("Please select the game you would like to play \n Game 1 \n Game 2\n Please enter the game as you see it: ")
     if game_selection == "Game 1":
-        import csv
+        #import csv
         game1 = open("Copy of Family Feud Question Database - Game 1.csv", newline = "")
+        print(game1)
     else:
         pass
     while name_selection == True:
